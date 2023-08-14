b3dc5bdfb115d02f02dd422c3cd7940c4fdca1ff
Haxor44 <evolmalek04@gmail.com>
2023-08-14 22:33:06 +0300

    cors-proxy!!!

---
+++ proxy/views.py
@@ -0,0 +1,17 @@
+import requests
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    url = "https://service-api-0nnq.onrender.com/api/v2/services"
+    response = requests.get(url)
+    if response.status_code == 200:
+        text = response.json()
+        print(text)
+        return HttpResponse(text)
+    else:
+        return HttpResponse("Cors proxy!!!")
